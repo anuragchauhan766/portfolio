3d9dcd30a907fc6ea311dcac1a5d1a893c14605b
Anurag Singh Chauhan <dev.anuragchauhan@gmail.com>
2024-07-04 15:11:59 +0530

    testing husky

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -24,7 +24,7 @@ export default function Home() {
               height={24}
               priority
             />
-          </a>  <p> hello</p>
+          </a><p> hello</p>
         </div>
       </div>
 
